d04157e58b4563656c4a8d639c6232e63b96b679
Holger Lembke <lembke@gmail.com>
2021-01-31 17:20:11 +0100

    0.9.8 first version with the new style

--- src/sendemail.cpp
+++ src/sendemail.cpp
@@ -1,176 +1,290 @@
 #include "sendemail.h"
 
-SendEmail::SendEmail(const String& host, const int port, const String& user, const String& passwd, const int timeout, const bool ssl) :
-    host(host), port(port), user(user), passwd(passwd), timeout(timeout), ssl(ssl), client((ssl) ? new WiFiClientSecure() : new WiFiClient())
-{
-
-}
-
-String SendEmail::readClient()
+String SendEmail::readClient(WiFiClient * client)
 {
   String r = client->readStringUntil('\n');
   r.trim();
-  while (client->available()) r += client->readString();
+  while (client->available()) {
+    r += client->readString();
+  }
   return r;
 }
 
-bool SendEmail::send(const String& from, const String& to, const String& subject, const String& msg)
+bool SendEmail::CallbackCall(maildatatype_t data) {
+  serverdata.data = data;
+  serverdata.value = "";
+  return datacallback(&serverdata);
+}
+
+bool SendEmail::send()
 {
-  if (!host.length())
-  {
+  bool more; // needed later
+
+  CallbackCall(ahost);
+  String host = serverdata.value;
+  CallbackCall(aport);
+  int port = serverdata.value.toInt();
+
+  if (!host.length()) {
     return false;
   }
-  client->stop();
-  client->setTimeout(timeout);
+  WiFiClient * client = new WiFiClient();
+
+  CallbackCall(atimeout);
+  client->setTimeout(serverdata.value.toInt());
+
   // smtp connect
 #ifdef DEBUG_EMAIL_PORT
-  DEBUG_EMAIL_PORT.print("Connecting: ");
+  DEBUG_EMAIL_PORT.print(F("Connecting: "));
   DEBUG_EMAIL_PORT.print(host);
-  DEBUG_EMAIL_PORT.print(":");
+  DEBUG_EMAIL_PORT.print(F(":"));
   DEBUG_EMAIL_PORT.println(port);
 #endif
-  if (!client->connect(host.c_str(), port))
-  {
+  if (!client->connect(host.c_str(), port)) {
+    client->stop();
+    delete client;
     return false;
   }
-  String buffer = readClient();
+  String buffer = readClient(client);
 #ifdef DEBUG_EMAIL_PORT
   DEBUG_EMAIL_PORT.println(buffer);
 #endif
-  if (!buffer.startsWith(F("220")))
-  {
+  if (!buffer.startsWith(F("220"))) {
+    client->stop();
+    delete client;
     return false;
   }
-  buffer = F("EHLO ");
-  buffer += client->localIP();
-  client->println(buffer);
+
+  client->print(F("EHLO "));
+  client->println(client->localIP().toString());
+#ifdef DEBUG_EMAIL_PORT
+  DEBUG_EMAIL_PORT.print(F("EHLO "));
+  DEBUG_EMAIL_PORT.println(client->localIP().toString());
+#endif
+  buffer = readClient(client);
 #ifdef DEBUG_EMAIL_PORT
   DEBUG_EMAIL_PORT.println(buffer);
 #endif
-  buffer = readClient();
-#ifdef DEBUG_EMAIL_PORT
-  DEBUG_EMAIL_PORT.println(buffer);
-#endif
-  if (!buffer.startsWith(F("250")))
-  {
+  if (!buffer.startsWith(F("250"))) {
+    client->stop();
+    delete client;
     return false;
   }
-  if (user.length()>0  && passwd.length()>0 )
+
   {
-    buffer = F("AUTH LOGIN");
-    client->println(buffer);
+    CallbackCall(auser);
+    String user = serverdata.value;
+
+    CallbackCall(apassword);
+    String passwd = serverdata.value;
+
+    if (user.length() > 0  && passwd.length() > 0 ) {
+      buffer = F("AUTH LOGIN");
+      client->println(buffer);
 #ifdef DEBUG_EMAIL_PORT
-  DEBUG_EMAIL_PORT.println(buffer);
+      DEBUG_EMAIL_PORT.println(buffer);
 #endif
-    buffer = readClient();
+      buffer = readClient(client);
 #ifdef DEBUG_EMAIL_PORT
-  DEBUG_EMAIL_PORT.println(buffer);
+      DEBUG_EMAIL_PORT.println(buffer);
 #endif
-    if (!buffer.startsWith(F("334")))
-    {
-      return false;
+      if (!buffer.startsWith(F("334"))) {
+        client->stop();
+        delete client;
+        return false;
+      }
+      base64 b;
+      buffer = user;
+      buffer = b.encode(buffer);
+      client->println(buffer);
+#ifdef DEBUG_EMAIL_PORT
+      DEBUG_EMAIL_PORT.println(buffer);
+#endif
+      buffer = readClient(client);
+#ifdef DEBUG_EMAIL_PORT
+      DEBUG_EMAIL_PORT.println(buffer);
+#endif
+      if (!buffer.startsWith(F("334"))) {
+        client->stop();
+        delete client;
+        return false;
+      }
+      buffer = passwd;
+      buffer = b.encode(buffer);
+      client->println(buffer);
+#ifdef DEBUG_EMAIL_PORT
+      DEBUG_EMAIL_PORT.println(buffer);
+#endif
+      buffer = readClient(client);
+#ifdef DEBUG_EMAIL_PORT
+      DEBUG_EMAIL_PORT.println(buffer);
+#endif
+      if (!buffer.startsWith(F("235"))) {
+        client->stop();
+        delete client;
+        return false;
+      }
     }
-    base64 b;
-    buffer = user;
-    buffer = b.encode(buffer);
-    client->println(buffer);
-#ifdef DEBUG_EMAIL_PORT
-  DEBUG_EMAIL_PORT.println(buffer);
-#endif
-    buffer = readClient();
-#ifdef DEBUG_EMAIL_PORT
-  DEBUG_EMAIL_PORT.println(buffer);
-#endif
-    if (!buffer.startsWith(F("334")))
-    {
-      return false;
-    }
-    buffer = this->passwd;
-    buffer = b.encode(buffer);
-    client->println(buffer);
-#ifdef DEBUG_EMAIL_PORT
-  DEBUG_EMAIL_PORT.println(buffer);
-#endif
-    buffer = readClient();
-#ifdef DEBUG_EMAIL_PORT
-  DEBUG_EMAIL_PORT.println(buffer);
-#endif
-    if (!buffer.startsWith(F("235")))
-    {
-      return false;
-    }
   }
-  // smtp send mail
-  buffer = F("MAIL FROM: ");
-  buffer += from;
-  client->println(buffer);
+
+  // Envelope begin
+
+  client->print(F("MAIL FROM: "));
+  CallbackCall(aenvelopefrom);
+  client->println(serverdata.value);
+#ifdef DEBUG_EMAIL_PORT
+  DEBUG_EMAIL_PORT.print(F("MAIL FROM: "));
+  DEBUG_EMAIL_PORT.println(serverdata.value);
+#endif
+  buffer = readClient(client);
 #ifdef DEBUG_EMAIL_PORT
   DEBUG_EMAIL_PORT.println(buffer);
 #endif
-  buffer = readClient();
-#ifdef DEBUG_EMAIL_PORT
-  DEBUG_EMAIL_PORT.println(buffer);
-#endif
-  if (!buffer.startsWith(F("250")))
-  {
+  if (!buffer.startsWith(F("250"))) {
+    client->stop();
+    delete client;
     return false;
   }
-  buffer = F("RCPT TO: ");
-  buffer += to;
-  client->println(buffer);
+
+  client->print(F("RCPT TO: "));
+  CallbackCall(aenvelopeto);
+  client->println(serverdata.value);
+#ifdef DEBUG_EMAIL_PORT
+  DEBUG_EMAIL_PORT.print(F("RCPT TO: "));
+  DEBUG_EMAIL_PORT.println(serverdata.value);
+#endif
+  buffer = readClient(client);
 #ifdef DEBUG_EMAIL_PORT
   DEBUG_EMAIL_PORT.println(buffer);
 #endif
-  buffer = readClient();
-#ifdef DEBUG_EMAIL_PORT
-  DEBUG_EMAIL_PORT.println(buffer);
-#endif
-  if (!buffer.startsWith(F("250")))
-  {
+  if (!buffer.startsWith(F("250"))) {
+    client->stop();
+    delete client;
     return false;
   }
+
+  // Envelope end, Mail starts
+
   buffer = F("DATA");
   client->println(buffer);
 #ifdef DEBUG_EMAIL_PORT
   DEBUG_EMAIL_PORT.println(buffer);
 #endif
-  buffer = readClient();
+  buffer = readClient(client);
 #ifdef DEBUG_EMAIL_PORT
   DEBUG_EMAIL_PORT.println(buffer);
 #endif
-  if (!buffer.startsWith(F("354")))
-  {
+  if (!buffer.startsWith(F("354"))) {
+    client->stop();
+    delete client;
     return false;
   }
-  buffer = F("From: ");
-  buffer += from;
-  client->println(buffer);
+
+  // Multiple Tos, & Ccs
+  for (int i = 0; i < 2; i++) {
+    serverdata.callcount = 0;
+    do {
+      serverdata.value = "";
+      if (i == 0) {
+        more = CallbackCall(ato);
+      } else {
+        more = CallbackCall(acc);
+      }
+      if (serverdata.value != "") {
+        if (serverdata.callcount == 0) {
+          if (i == 0) {
+            client->print(F("To: "));
+          } else {
+            client->print(F("Cc: "));
+          }
+        }
+        serverdata.callcount++;
+        client->print(serverdata.value);
+        if (more) {
+          client->print(", ");
+        }
 #ifdef DEBUG_EMAIL_PORT
-  DEBUG_EMAIL_PORT.println(buffer);
+        if (i == 0) {
+          DEBUG_EMAIL_PORT.print(F("To: "));
+        } else {
+          DEBUG_EMAIL_PORT.print(F("Cc: "));
+        }
+        DEBUG_EMAIL_PORT.println(serverdata.value);
 #endif
-  buffer = F("To: ");
-  buffer += to;
-  client->println(buffer);
+      }
+    } while (more); // should check connected-status, too
+    if (serverdata.callcount != 0) {
+      client->println();
+    }
+  }
+
+  client->print(F("From: "));
+  CallbackCall(afrom);
+  client->println(serverdata.value);
 #ifdef DEBUG_EMAIL_PORT
-  DEBUG_EMAIL_PORT.println(buffer);
+  DEBUG_EMAIL_PORT.print(F("From: "));
+  DEBUG_EMAIL_PORT.println(serverdata.value);
 #endif
-  buffer = F("Subject: ");
-  buffer += subject;
-  buffer += F("\r\n");
-  client->println(buffer);
+
+  client->print(F("Subject: "));
+  CallbackCall(asubject);
+  client->println(serverdata.value);
 #ifdef DEBUG_EMAIL_PORT
-  DEBUG_EMAIL_PORT.println(buffer);
+  DEBUG_EMAIL_PORT.print(F("Subject: "));
+  DEBUG_EMAIL_PORT.println(serverdata.value);
 #endif
-  buffer = msg;
-  client->println(buffer);
+
+  // extra headers?
+  serverdata.callcount = 0;
+  more = CallbackCall(axtraheader);
+  if (serverdata.value != "") {
+    client->println(serverdata.value);
+    serverdata.callcount++;
+#ifdef DEBUG_EMAIL_PORT
+    DEBUG_EMAIL_PORT.println(serverdata.value);
+#endif
+    while (more) {
+      more = CallbackCall(axtraheader);
+      client->println(serverdata.value);
+      serverdata.callcount++;
+#ifdef DEBUG_EMAIL_PORT
+      DEBUG_EMAIL_PORT.println(serverdata.value);
+#endif
+    }
+  }
+
+  // End of Header
+  client->println(F("\r\n"));
+
+  // Fetch multiple body lines.
+  serverdata.data = amessage;
+  serverdata.callcount = 0;
+
+#ifdef DEBUG_EMAIL_PORT
+  DEBUG_EMAIL_PORT.println(F("Message body"));
+#endif
+  do {
+    serverdata.value = "";
+    more = messagebodycallback(&serverdata);
+    serverdata.callcount++;
+    client->println(serverdata.value);
+#ifdef DEBUG_EMAIL_PORT
+    DEBUG_EMAIL_PORT.println(serverdata.value);
+#endif
+  } while (more); // should check connected-status, too
+
+  // Bye.
   client->println('.');
+
+  // Now. Bye.
+  client->println(F("QUIT"));
 #ifdef DEBUG_EMAIL_PORT
-  DEBUG_EMAIL_PORT.println(buffer);
+  DEBUG_EMAIL_PORT.println(F("QUIT"));
 #endif
-  buffer = F("QUIT");
-  client->println(buffer);
-#ifdef DEBUG_EMAIL_PORT
-  DEBUG_EMAIL_PORT.println(buffer);
-#endif
+
+  client->stop();
+  delete client;
+
   return true;
 }
+
--- src/sendemail.h
+++ src/sendemail.h
@@ -1,27 +1,63 @@
 #ifndef __SENDEMAIL_H
 #define __SENDEMAIL_H
 
-//#define DEBUG_EMAIL_PORT
+#define DEBUG_EMAIL_PORT Serial
 
+#include <Arduino.h>
 #include <WiFiClient.h>
 #include <WiFiClientSecure.h>
 #include <base64.h>
 
+
+
 class SendEmail
 {
+  public:
+    // Servercommunication
+    enum maildatatype_t { // login
+                          ahost, aport, auser, apassword, atimeout, ausessl,
+                          // Mail-Envelope
+                          aenvelopefrom, aenvelopeto, 
+                          // Mail
+                          afrom, ato /*x*/, acc /*x*/,
+                          asubject, amessage /*x*/,
+                          axtraheader /*x*/ };
+/*  Adresses are either
+      localpart@example.invalid
+     or
+      some real name <localpart@example.invalid>
+    Strings will be added into the mail as they are. Give your best and make it work.  
+    BCC is aenvelopeto...
+    
+    Field with (*x*) can have multiple calls
+*/
+    struct data_t {
+        maildatatype_t data;
+        int callcount;
+        String value;    
+    } serverdata;
+    // for .....: true: more to come, false: nothing more
+    // else ignored.
+    typedef bool (*servercommunication_t) (SendEmail::data_t * data);
+    servercommunication_t datacallback;
+    
+    // mail body gets an extra callback to make it cosy
+    typedef bool (*mailcommunication_t) (SendEmail::data_t * data);
+    mailcommunication_t messagebodycallback;
+    
   private:
+    /*
     const String host;
     const int port;
     const String user;
     const String passwd;
     const int timeout;
     const bool ssl;
-    WiFiClient* client;
-    String readClient();
+    */
+    bool CallbackCall(maildatatype_t data);
+    String readClient(WiFiClient * client);
   public:
-   SendEmail(const String& host, const int port, const String& user, const String& passwd, const int timeout, const bool ssl);
-   bool send(const String& from, const String& to, const String& subject, const String& msg);
-   ~SendEmail() {client->stop(); delete client;}
+   bool send();
 };
 
 #endif
